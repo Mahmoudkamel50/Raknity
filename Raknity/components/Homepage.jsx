cc70da1566dfc1b09ada98439ece1acf88efbc78
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-05-13 00:57:05 +0200

    Added a Picker to choose a government (No styling yet)

--- Raknity/components/Homepage.jsx
+++ Raknity/components/Homepage.jsx
@@ -1,10 +1,37 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getGovts } from '../dataBase/APIFunctions';
+import {Picker} from '@react-native-picker/picker';
 
 const Homepage = () => {
+
+  useEffect(() => {
+    getGovts().then((data) => {
+      setGovts(data);
+    })
+  }, [])
+  
+  const [govts, setGovts] = useState([]);
+  const [chosenGovt, setChosenGovt] = useState("");
+
   return (
-    <View>
-      <Text>Homepage</Text>
+    <View style={{flexDirection: 'row'}}>
+      <Text>Choose a goverment:</Text>
+      <Picker
+        selectedValue={chosenGovt}
+        onValueChange={(govt, index) => {
+          setChosenGovt(govt);
+        }}
+      >
+        <Picker.Item label='Nothing selected' value={""}/>
+        {
+          govts.map((e, index) => {
+            return (
+              <Picker.Item label={e.id} value={e.id} key={index}/>
+            );
+          })
+        }
+      </Picker>
     </View>
   )
 }
