51baa11b7cd8901d3ec6081c7a9d4cc2b18d3f4b
Mahmoudkamel50 <mk687904@gmail.com>
2022-05-12 14:24:14 +0200

    signUp component with authentication flow

---
+++ Raknity/components/signUp.jsx
@@ -0,0 +1,110 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { register, getUserUId } from "../dataBase/authorization";
+import { addUser, getUserById } from "../dataBase/user";
+
+const routeName = "SignUp";
+
+export { routeName };
+
+export default function SignUp({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function registerUser() {
+    if (email === "" || password === "") {
+      alert("email or password is empty!");
+    } else {
+      register(email, password)
+        .then(() => {
+          console.log(getUserUId());
+
+          getUserUId().then((id) => {
+            // console.log(id);
+            addUser({ id: id, email, password });
+          });
+          navigation.navigate("SignIn");
+        })
+        .catch((e) => {
+          console.log(e.message);
+        });
+    }
+  }
+
+  return (
+    <ScrollView style={{ padding: 30 }}>
+      <View style={{ padding: 10 }}>
+        <Text>Email</Text>
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <TextInput
+          placeholder={"Enter your email"}
+          onChangeText={setEmail}
+          value={email}
+        />
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <Text>Password</Text>
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <TextInput
+          placeholder={"Enter your Password"}
+          secureTextEntry={true}
+          onChangeText={setPassword}
+          value={password}
+        />
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <Text>Confirm Password</Text>
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <TextInput placeholder={"Enter your Password again"} />
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <Text>First Name</Text>
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <TextInput placeholder={"Enter your First Name"} />
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <Text>Last Name</Text>
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <TextInput placeholder={"Enter your Last Name"} />
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <Text>Phone</Text>
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <TextInput placeholder={"Enter your Phone"} />
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <Button title={"Create Account"} onPress={registerUser} />
+      </View>
+
+      <StatusBar style="auto" />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({});
