afe571254ae9af7e69088661681693ef04f0f003
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-05-12 14:30:37 +0200

    Deleted authorization folder

--- Raknity/Authorization/SignIn.js
+++
@@ -1,8 +0,0 @@
-import { auth } from "../dataBase/configuration";
-import { signInWithEmailAndPassword } from "firebase/auth";
-
-async function signIn(email, password) {
-    await signInWithEmailAndPassword(auth, email, password);
-}
-
-export {signIn};
