905bbc9ef0e8ee1a3265a957a35e7b4b6d1a5425
Mahmoudkamel50 <mk687904@gmail.com>
2022-05-13 19:29:57 +0200

    reset signUp

--- Raknity/components/signUp.jsx
+++ Raknity/components/signUp.jsx
@@ -15,6 +15,10 @@ import SignIn from "./SignIn";
 export default function SignUp({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLasttName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [history, setHistory] = useState([]);
 
   function registerUser() {
     if (email === "" || password === "") {
@@ -23,10 +27,17 @@ export default function SignUp({ navigation }) {
       register(email, password)
         .then(() => {
           console.log(getUserUId());
- 
+
           getUserUId().then((id) => {
             // console.log(id);
-            addUser({ id: id, email, password });
+            addUser({
+              id: id,
+              email,
+              firstName,
+              lastName,
+              phoneNumber,
+              history,
+            });
           });
           navigation.navigate("Sign in");
         })
@@ -40,87 +51,63 @@ export default function SignUp({ navigation }) {
     <View style={styles.container}>
       <ScrollView>
         <View style={styles.firstnameInput}>
-          <Text
-            style={styles.text}
-          >
-            First Name:
-          </Text>
+          <Text style={styles.text}>First Name:</Text>
           <TextInput
             placeholder={"Enter your First Name"}
-            textAlign='center'
+            textAlign="center"
             style={styles.textInputsname}
           />
-          </View>
-          <View style={styles.lastnameinput}>
-          <Text
-            style={styles.text}
-          >
-            Last Name:
-          </Text>
+        </View>
+        <View style={styles.lastnameinput}>
+          <Text style={styles.text}>Last Name:</Text>
           <TextInput
             placeholder={"Enter your Last Name"}
-            textAlign='center'
+            textAlign="center"
             style={styles.textInputsname}
           />
         </View>
 
         <View style={styles.inputs}>
-          <Text
-            style={styles.text}
-          >
-            Email:
-          </Text>
+          <Text style={styles.text}>Email:</Text>
           <TextInput
             placeholder={"Enter your email"}
             onChangeText={setEmail}
             value={email}
-            textAlign='center'
+            textAlign="center"
             style={styles.textInputs}
           />
         </View>
         <View style={styles.inputs}>
-          <Text
-            style={styles.text}
-          >
-            Password:
-          </Text>
+          <Text style={styles.text}>Password:</Text>
           <TextInput
             placeholder={"Enter your Password"}
             secureTextEntry={true}
             onChangeText={setPassword}
             value={password}
-            textAlign='center'
+            textAlign="center"
             style={styles.textInputs}
           />
         </View>
         <View style={styles.inputs}>
-          <Text
-            style={styles.text}
-          >
-            Confirm Password:
-          </Text>
+          <Text style={styles.text}>Confirm Password:</Text>
           <TextInput
             placeholder={"Enter your Password again"}
             secureTextEntry={true}
-            textAlign='center'
+            textAlign="center"
             style={styles.textInputs}
           />
         </View>
         <View style={styles.inputs}>
-          <Text
-            style={styles.text}
-          >
-            Phone:
-          </Text>
+          <Text style={styles.text}>Phone:</Text>
           <TextInput
             placeholder={"Enter your Phone"}
-            textAlign='center'
+            textAlign="center"
             style={styles.textInputs}
           />
         </View>
 
         <View style={styles.buttonview}>
-        <Button title={"Create Account"} onPress={registerUser} />
+          <Button title={"Create Account"} onPress={registerUser} />
         </View>
 
         <StatusBar style="auto" />
@@ -132,40 +119,40 @@ export default function SignUp({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     paddingTop: 50,
-    alignItems: 'center',
+    alignItems: "center",
     flex: 1,
   },
   inputs: {
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 10,
   },
   firstnameInput: {
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 10,
   },
   lastnameinput: {
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 10,
   },
   text: {
-    padding: 10
+    padding: 10,
   },
   textInputsname: {
-    backgroundColor: '#d3d3d3',
-    width: '100%',
+    backgroundColor: "#d3d3d3",
+    width: "100%",
     borderRadius: 40,
     width: 200,
-    padding:10,
+    padding: 10,
   },
   textInputs: {
-    backgroundColor: '#d3d3d3',
-    width: '100%',
+    backgroundColor: "#d3d3d3",
+    width: "100%",
     borderRadius: 40,
     width: 250,
     padding: 10,
   },
-  buttonview:{
+  buttonview: {
     alignItems: "center",
     padding: 20,
-  }
+  },
 });
