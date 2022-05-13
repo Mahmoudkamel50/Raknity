ca9c0c7e85c37e5c4391422744b62d90bb88ae0b
Mahmoudkamel50 <mk687904@gmail.com>
2022-05-13 20:09:53 +0200

    updated profile

--- Raknity/components/Profile.jsx
+++ Raknity/components/Profile.jsx
@@ -1,54 +1,72 @@
-import {StatusBar} from 'expo-status-bar';
-import {Button, ScrollView, StyleSheet, View, Text, TextInput} from 'react-native';
-import {getUsers} from "../dataBase/user";
-import {getUserUId} from "../dataBase/authorization";
-import {getUserById} from "../dataBase/user";
-import {useState} from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+} from "react-native";
+import { getUsers } from "../dataBase/user";
+import { getUserUId } from "../dataBase/authorization";
+import { getUserById } from "../dataBase/user";
+import { useState, useEffect } from "react";
 import * as React from "react";
 
-const routeName = 'Profile';
+const routeName = "Profile";
 
-export {routeName};
+export { routeName };
 
-export default function Profile({navigation}) {
+export default function Profile({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [id, setId] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [id, setId] = useState('');
+  useEffect(() => {
+    getUserUId().then((id) => {
+      console.log(id);
+      getUserById(id).then((user) => {
+        setEmail(user[0].email);
+        setPhoneNumber(user[0].phoneNumber);
+        setId(user[0].id);
+        setFirstName(user[0].firstName);
+        setLastName(user[0].lastName);
+      });
+    });
+  }, []);
 
-    React.useEffect(() => {
-        getUserUId().then((id) => {
-            console.log(id);
-            getUserById(id).then((user)=>{
-                setEmail(user[0].email);
-                setPassword(user[0].password);
-                setId(user[0].id);
-            })
-        });
-    }, []);
+  return (
+    <ScrollView style={{ padding: 30 }}>
+      <View style={{ padding: 10 }}>
+        <Text>First Name</Text>
+        <Text>{firstName}</Text>
+      </View>
 
-    return (
-        <ScrollView style={{padding: 30}}>
+      <View style={{ padding: 10 }}>
+        <Text>Last Name</Text>
+        <Text>{lastName}</Text>
+      </View>
 
-            <View style={{padding: 10}}>
-                <Text>email</Text>
-                <Text>{email}</Text>
-            </View>
+      <View style={{ padding: 10 }}>
+        <Text>E-mail</Text>
+        <Text>{email}</Text>
+      </View>
 
-            <View style={{padding: 10}}>
-                <Text>password</Text>
-                <Text>{password}</Text>
-            </View>
+      <View style={{ padding: 10 }}>
+        <Text>id</Text>
+        <Text>{id}</Text>
+      </View>
 
-            <View style={{padding: 10}}>
-                <Text>id</Text>
-                <Text>{id}</Text>
-            </View>
+      <View style={{ padding: 10 }}>
+        <Text>Phone Number</Text>
+        <Text>{phoneNumber}</Text>
+      </View>
 
-
-            <StatusBar style="auto"/>
-        </ScrollView>
-    );
+      <StatusBar style="auto" />
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({});
