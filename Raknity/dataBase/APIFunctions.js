0367085c31efbc48091b817a7550823394dacc46
Mahmoudkamel50 <mk687904@gmail.com>
2022-05-13 19:58:11 +0200

    updated screen

--- Raknity/dataBase/APIFunctions.js
+++ Raknity/dataBase/APIFunctions.js
@@ -2,32 +2,31 @@ import { db } from "./configuration";
 import { getDocs, doc, collection, getDoc } from "firebase/firestore";
 
 async function getGovts() {
-    const locCol = collection(db, 'locations');
-    const locSnapshot = await getDocs(locCol);
-    const locList = locSnapshot.docs.map((doc) => {
-        return { id:doc.id, ...doc.data()};
-    });
-    console.log(locList);
-    return locList;
+  const locCol = collection(db, "locations");
+  const locSnapshot = await getDocs(locCol);
+  const locList = locSnapshot.docs.map((doc) => {
+    return { id: doc.id, ...doc.data() };
+  });
+  console.log(locList);
+  return locList;
 }
 
 async function getCities(id) {
-    const allCities = await getGovts();
-    console.log(allCities);
-    let wantedData;
-    for (let i = 0; i < allCities.length; i++) {
-        if (allCities[i].id == id) {
-            wantedData = allCities[i];
-        }
+  const allCities = await getGovts();
+  console.log(allCities);
+  let wantedData;
+  for (let i = 0; i < allCities.length; i++) {
+    if (allCities[i].id == id) {
+      wantedData = allCities[i];
     }
-    return wantedData;
+  }
+  return wantedData;
 }
-
 async function getGovt(id) {
-    const locCol = collection(db, "locations");
-    const doc = await getDoc(locCol, id);
-    console.log(doc);
-    return doc;
+  const locCol = collection(db, "locations");
+  const doc = await getDoc(locCol, id);
+  console.log(doc);
+  return doc;
 }
 
-export { getGovts , getCities, getGovt};
+export { getGovts, getCities, getGovt };
