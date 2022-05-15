18cf81357864f41e340bfdce5cd5ed32ea55f088
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-05-15 04:03:38 +0200

    restyling headers

--- Raknity/components/Stacks/AuthorizationStack.jsx
+++ Raknity/components/Stacks/AuthorizationStack.jsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignUp  from '../signUp';
+import SignUp from '../signUp';
 import SignIn from '../SignIn';
 
 const AuthStack = createNativeStackNavigator();
@@ -11,12 +11,26 @@ const AuthorizationStack = () => {
 
   return (
     <NavigationContainer>
-        <AuthStack.Navigator initialRouteName='Sign in'>
-            
-            <AuthStack.Screen name='Sign in' component={SignIn}/>
-            <AuthStack.Screen name='Sign up' component={SignUp}/>
-
-        </AuthStack.Navigator>
+      <AuthStack.Navigator initialRouteName='Sign in'>
+        <AuthStack.Screen
+          name='Sign in'
+          component={SignIn}
+          options={{
+            headerStyle: {
+              backgroundColor: '#3ded97'
+            }
+          }}
+        />
+        <AuthStack.Screen
+          name='Sign up'
+          component={SignUp}
+          options={{
+            headerStyle: {
+              backgroundColor: '#3ded97'
+            }
+          }}
+        />
+      </AuthStack.Navigator>
     </NavigationContainer>
   )
 }
