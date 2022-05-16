ef6d21f1eeed0039bad8237504b59ca3b4bc07b9
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-05-17 01:12:19 +0200

    restyling sign in page

--- Raknity/components/SignIn.jsx
+++ Raknity/components/SignIn.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput, View, Text } from "react-native";
+import { StyleSheet, TextInput, View, Text, Image } from "react-native";
 import React, { useState, useContext } from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { AuthContext } from "../dataBase/Utils";
@@ -7,13 +7,22 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { login } from "../dataBase/authorization";
 import { StatusBar } from "expo-status-bar";
 
-const SignIn = ({navigation}) => {
+const SignIn = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
   return (
     <View style={styles.container}>
+      <View style={{ alignItems: 'center' }}>
+        <Image source={require('../assets/Final-Raknity-logo.png')} style={styles.img} />
+      </View>
+      <View style={{ alignItems: 'center' }}>
+        <Text style={styles.welcomeTxt}> Welcome to Raknity!</Text>
+      </View>
+      <View style={{ alignItems: 'center', padding: 10 }}>
+        <Text style={styles.credTxt}> Please, enter your credentials</Text>
+      </View>
       <View style={styles.input}>
         <Text style={{ fontSize: 17, color: '#fff', padding: 5 }}>E-mail: </Text>
         <View style={styles.textInput}>
@@ -51,17 +60,19 @@ const SignIn = ({navigation}) => {
               .catch((error) => setError(error.message));
           }}
         >
-          <Text>Sign in</Text>
+          <Text style={styles.btText}>Sign in</Text>
         </Icon.Button>
         <Text style={styles.error}>{error}</Text>
       </View>
-      <Text
-        style={styles.noAccount}
-        onPress={() => navigation.navigate("Sign up")}
-      >
-        Don't have an account?
-      </Text>
-      <StatusBar style="inverted"/>
+      <View style={{ alignItems: 'center' }}>
+        <Text
+          style={styles.noAccount}
+          onPress={() => navigation.navigate("Sign up")}
+        >
+          Don't have an account?
+        </Text>
+      </View>
+      <StatusBar style="inverted" />
     </View>
   );
 };
@@ -72,7 +83,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 30,
-    alignItems: "center",
     flexDirection: "column",
     backgroundColor: '#151e3d'
   },
@@ -101,4 +111,23 @@ const styles = StyleSheet.create({
     color: "red",
     paddingTop: 10,
   },
+  img: {
+    height: 250,
+    width: 250,
+    marginBottom: 10,
+  },
+  welcomeTxt: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingBottom: 10,
+    color: '#3ded97',
+  },
+  credTxt: {
+    fontSize: 20,
+    paddingBottom: 10,
+    color: '#3ded97',
+  },
+  btText: {
+    color: '#151e3d'
+  }
 });
